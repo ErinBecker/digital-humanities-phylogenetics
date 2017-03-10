7deada38c396b9edb7e87c827a1420e029cfc31d
ErinBecker <erinstellabecker@gmail.com>
2017-03-10 13:17:53 -0800

    Do some stuff in RStudio because Jupyter Notebooks broke

---
+++ scripts/sectioning_method_2.R
@@ -0,0 +1,75 @@
+# Use getLongestCommonSubstring() from Rlibstree
+# source("http://bioconductor.org/biocLite.R")
+# biocLite("Rlibstree")
+
+# Needs bioconductor and having difficulty installing on Jupyter notebook
+# Want to test and make sure it works before I go through all the trouble
+# setwd("~/Box Sync/digital-humanities-phylogenetics/data/")
+
+library(Rlibstree)
+
+df_composite = read.csv(file = "composite_df.csv", stringsAsFactors = FALSE)
+# remove rows representing missing lines
+
+df_kmers = data.frame(line_a = character(nrow(df_composite)), 
+                      line_b = character(nrow(df_composite)),
+                      kmer = character(nrow(df_composite)),
+                      kmer2 = character(nrow(df_composite)),
+                      kmer3 = character(nrow(df_composite)),
+                      k = numeric(nrow(df_composite)),
+                      stringsAsFactors=FALSE)
+
+for(i in 1:nrow(df_composite)) {
+  line_a = tolower(df_composite$entry[i])
+  line_b = tolower(df_composite$entry[i +1])
+  kmer = getLongestCommonSubstring(c(line_a, line_b))
+  kmer2 = gsub("[\x80-\xFF]", "", kmer)  # what other special characters?
+  kmer3 = gsub("\\]", "", kmer2) # get rid of punctuation (but only {}[]_.)
+  kmer3 = gsub("\\[", "", kmer3)
+  kmer3 = gsub("\\{", "", kmer3)
+  kmer3 = gsub("\\}", "", kmer3)
+  kmer3 = gsub("_", "", kmer3)
+  kmer3 = gsub("\\.", "", kmer3)
+  k = nchar(kmer3)
+  df_kmers$line_a[i] = line_a
+  df_kmers$line_b[i] = line_b
+  df_kmers$kmer[i] = kmer
+  df_kmers$kmer2[i] = kmer2
+  df_kmers$kmer3[i] = kmer3
+  df_kmers$k[i] = k
+  df_kmers
+}
+
+plot(df_kmers$k, type = "l")
+plot(df_kmers$k, pch = ".")
+
+# Get changepoints to determine section breaks
+# install.packages("changepoint")
+# library(changepoint)
+
+# section_boundaries = cpt.var(df_kmers$k, penalty = "None",
+#                              Q = 20)
+
+# Things to check:
+
+# 1) There are two lines in the composite text which represent 
+# missing lines. This seems strange to me. 
+# which(df_composite$entry == "")
+
+# 2) getLongestCommonSubstring sticks a seemingly random additional
+# character onto the end of the uncovered string.
+# sometimes these characters are multibyte strings and can't be
+# handled by nchar
+# Fixed this by gsubing out multibyte characters
+# View(df_kmers[which(df_kmers$kmer != df_kmers$kmer2),])
+
+#3) Even after fixing #2, kmer length is not reproducible
+# running the code that creates df_kmers multiple times 
+# produces slight differences
+# look into this
+
+#4) Remove punctuation and set cutoff to 3 (anything below 3 is a section break)
+#5) If entire guide word (between []) or the entire citation form (everything before [) matches, keep as part of section
+#6) take out "nana" kmers
+#7) take out part of speech?
+
