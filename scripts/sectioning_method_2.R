53cb9bbb1b85466fbdeeeffedc97601de45ba073
ErinBecker <erinstellabecker@gmail.com>
2017-03-15 15:39:27 -0700

    functionize and build df_kmers for 4 composites

--- scripts/sectioning_method_2.R
+++ scripts/sectioning_method_2.R
@@ -9,51 +9,52 @@ setwd("~/Box Sync/digital-humanities-phylogenetics/data/composite_texts/")
 
 library(Rlibstree)
 
-# separate data from composites.csv into one csv for each composite text
-composites = read.csv(file = "composites.csv", stringsAsFactors = FALSE)
-composites$X = NULL
-composites$document = gsub("(.*)\\..*", "\\1", composites$id_line)
-unique_docs = unique(composites$document)
-sapply(unique_docs, function(x) {
-  lines = which(composites$document == x)
-  write.csv(composites[lines,], file = paste0(x, ".csv"), quote = FALSE, row.names = FALSE)
-})
+# # separate data from composites.csv into one csv for each composite text
+# composites = read.csv(file = "composites.csv", stringsAsFactors = FALSE)
+# composites$X = NULL
+# composites$document = gsub("(.*)\\..*", "\\1", composites$id_line)
+# composites$entry = composites$lemma
+# unique_docs = unique(composites$document)
+# sapply(unique_docs, function(x) {
+#   lines = which(composites$document == x)
+#   write.csv(composites[lines,], file = paste0(x, ".csv"), quote = FALSE, row.names = FALSE)
+# })
 
-
-df_composite = read.csv(file = "Q000039.csv", stringsAsFactors = FALSE)
-# still need to remove rows representing missing lines
-
-df_kmers = data.frame(line_a = character(nrow(df_composite)), 
-                      line_b = character(nrow(df_composite)),
-                      kmer = character(nrow(df_composite)),
-                      kmer2 = character(nrow(df_composite)),
-                      kmer3 = character(nrow(df_composite)),
-                      k = numeric(nrow(df_composite)),
-                      stringsAsFactors=FALSE)
-
-for(i in 1:nrow(df_composite)) {
-  line_a = tolower(df_composite$entry[i])
-  line_b = tolower(df_composite$entry[i +1])
-  kmer = getLongestCommonSubstring(c(line_a, line_b))
-  kmer2 = gsub("[\x80-\xFF]", "", kmer)  # what other special characters?
-  kmer3 = gsub("\\]", "", kmer2) # get rid of punctuation (but only {}[]_.)
-  kmer3 = gsub("\\[", "", kmer3)
-  kmer3 = gsub("\\{", "", kmer3)
-  kmer3 = gsub("\\}", "", kmer3)
-  kmer3 = gsub("_", "", kmer3)
-  kmer3 = gsub("\\.", "", kmer3)
-  k = nchar(kmer3)
-  df_kmers$line_a[i] = line_a
-  df_kmers$line_b[i] = line_b
-  df_kmers$kmer[i] = kmer
-  df_kmers$kmer2[i] = kmer2
-  df_kmers$kmer3[i] = kmer3
-  df_kmers$k[i] = k
+create_kmers_df = function(file) {
+  df_composite = read.csv(file, stringsAsFactors = FALSE)
+  df_kmers = data.frame(line_a = character(nrow(df_composite)), 
+                        line_b = character(nrow(df_composite)),
+                        kmer = character(nrow(df_composite)),
+                        kmer2 = character(nrow(df_composite)),
+                        kmer3 = character(nrow(df_composite)),
+                        k = numeric(nrow(df_composite)),
+                        stringsAsFactors=FALSE)
+  
+  for(i in 1:nrow(df_composite)-1) {
+    line_a = tolower(df_composite$entry[i])
+    line_b = tolower(df_composite$entry[i +1])
+    kmer = getLongestCommonSubstring(c(line_a, line_b))
+    kmer2 = gsub("[\x80-\xFF]", "", kmer)  # what other special characters?
+    kmer3 = gsub("\\]", "", kmer2) # get rid of punctuation (but only {}[]_.)
+    kmer3 = gsub("\\[", "", kmer3)
+    kmer3 = gsub("\\{", "", kmer3)
+    kmer3 = gsub("\\}", "", kmer3)
+    kmer3 = gsub("_", "", kmer3)
+    kmer3 = gsub("\\.", "", kmer3)
+    k = nchar(kmer3)
+    df_kmers$line_a[i] = line_a
+    df_kmers$line_b[i] = line_b
+    df_kmers$kmer[i] = kmer
+    df_kmers$kmer2[i] = kmer2
+    df_kmers$kmer3[i] = kmer3
+    df_kmers$k[i] = k
+    df_kmers
+  }
+  plot(df_kmers$k, type = "l")
+  plot(df_kmers$k, pch = ".")
   df_kmers
 }
 
-plot(df_kmers$k, type = "l")
-plot(df_kmers$k, pch = ".")
 
 # Get changepoints to determine section breaks
 # install.packages("changepoint")
