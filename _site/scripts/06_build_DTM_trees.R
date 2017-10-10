cc038b4c93b71d7ac85c54316b85214ef58997cc
Erin Becker <erinstellabecker@gmail.com>
2017-10-10 13:52:28 -0700

    add intro ten simple rules

---
+++ _site/scripts/06_build_DTM_trees.R
@@ -0,0 +1,18 @@
+setwd("~/Box Sync/digital-humanities-phylogenetics/")
+
+all_pairs_matrix2 = read.csv("data/dtm_data/all_pairs_matrix2.csv", row.names = 1)
+
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("BiocInstaller")
+#biocLite("phangorn")
+
+library("phangorn")
+
+# Build UPGMA and neighbor joining trees
+treeUPGMA <- upgma(dist(all_pairs_matrix2))
+treeNJ  <- NJ(dist(all_pairs_matrix2))
+plot(treeUPGMA, main="UPGMA")
+plot(treeNJ, main = "Neighbor Joining")
+
+write.tree(treeUPGMA, "data/trees/dtm_upgma.tre")
+write.tree(treeNJ, "data/trees/dtm_nj.tre")
